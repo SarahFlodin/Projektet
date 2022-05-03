02c1a5fad2a3c4ed960870b44b88da1bb30619f9
Joakim Fröjvall Bejram <joakimfrojram@gmail.com>
2022-05-03 19:23:17 +0200

    måste kolla denna på open lab
    skapa en loop utav detta, sedan kunna framkalla de och göra de till knappar i js

---
+++ createFunctionCountryButtons.js
@@ -0,0 +1,38 @@
+"use strict";
+
+let countries= document.createElement("div");
+countries.innerText = "Argentina";
+document.body.appendChild(countries);
+    
+let countries1= document.createElement("div");
+countries1.innerText = "Australien";
+document.body.appendChild(countries1);
+
+let countries2= document.createElement("div");
+countries2.innerText = "Chile";
+document.body.appendChild(countries2);
+
+let countries3= document.createElement("div");
+countries3.innerText = "Frankrike";
+document.body.appendChild(countries3);
+
+let countries4= document.createElement("div");
+countries4.innerText = "Mexico";
+document.body.appendChild(countries4);
+
+let countries5= document.createElement("div");
+countries5.innerText = "Spanien";
+document.body.appendChild(countries5);
+
+let countries6= document.createElement("div");
+countries6.innerText = "Storbritannien";
+document.body.appendChild(countries6);
+
+let countries7= document.createElement("div");
+countries7.innerText = "Sverige";
+document.body.appendChild(countries7);
+
+let countries8= document.createElement("div");
+countries8.innerText = "Usa";
+document.body.appendChild(countries8);
+ 

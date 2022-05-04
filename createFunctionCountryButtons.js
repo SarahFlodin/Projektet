847613e78a74718722dccf6ce3951d47cd082ccc
Joakim Fröjvall Bejram <95027528+Jocke1999@users.noreply.github.com>
2022-05-04 10:59:29 +0200

    Create function country buttons (#82)
    * måste kolla denna på open lab
    
    skapa en loop utav detta, sedan kunna framkalla de och göra de till knappar i js
    
    * createdFunctionCountryButtons
    
    #61 finitio
    
    * Update .DS_Store
    
    Co-authored-by: Sarah Flodin Jakobsson <95027610+SarahFlodin@users.noreply.github.com>

---
+++ createFunctionCountryButtons.js
@@ -0,0 +1,59 @@
+"use strict";
+
+const COUNTRYBUTTON = [
+    {
+        "id": 0,
+        "name": "Spain"
+    },
+    {
+        "id": 1,
+        "name": "France"
+    },
+    {
+        "id": 2,
+        "name": "Australia"
+    },
+    {
+        "id": 3,
+        "name": "United Kingdom"
+    },
+    {
+        "id": 4,
+        "name": "Sweden"
+    },
+    {
+        "id": 5,
+        "name": "Mexico"
+    },
+    {
+        "id": 6,
+        "name": "USA"
+    },
+    {
+        "id": 7,
+        "name": "Argentina"
+    },
+    {
+        "id": 8,
+        "name": "Chile"
+    },
+    
+];
+
+
+const countryButtons = document.getElementById("countryButtons");
+
+function createCountryButtons (COUNTRYBUTTON) {
+    for (let i = 0; i < COUNTRYBUTTON.length; i++) {
+        const button = document.createElement("button");
+        button.innerText = ` ${COUNTRYBUTTON[i].name} `;
+        button.addEventListener("click", function() {
+            console.log(i)
+        })
+        countryButtons.appendChild(button);
+        }
+
+}
+
+createCountryButtons (COUNTRYBUTTON);
+    

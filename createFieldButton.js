0c6f450252700c88fd0b0b21301abc681d450d8f
melissasjostedt <97900446+melissasjostedt@users.noreply.github.com>
2022-05-04 12:00:55 +0200

    created buttons for programmePage
    #66

---
+++ createFieldButton.js
@@ -0,0 +1,47 @@
+"use script";
+
+const FIELDSBUTTON = [
+   {
+     "id": 0,
+     "name": "Matematik"
+   },
+
+  { "id": 1,
+    "name": "Teknik"
+  },
+
+ { "id": 2,
+   "name": "Juridik"
+ },
+
+  { "id": 3,
+   "name": "Medicin"
+  },
+
+ { "id": 4,
+   "name": "Sociologi"
+ },
+
+  { "id": 5,
+   "name": "Filosofi"
+  },
+
+   { "id": 6,
+   "name": "Design"
+   },
+
+];
+
+const fieldButtons = document.getElementById("fieldButtons");
+
+function createFieldButtons (FIELDSBUTTON) {
+    for (let i = 0; i < FIELDSBUTTON.length; i ++) {
+        const button = document.createElement("button");
+        button.innerText = `${FIELDSBUTTON[i].name}`;
+        button.addEventListener("click", function() {
+           console.log(i)
+        })
+        fieldButtons.appendChild(button);
+      } 
+}
+createFieldButtons (FIELDSBUTTON);

a780ba77270fc44ba7d772a03e7c77aef4a9642f
Joakim Fröjvall Bejram <joakimfrojram@gmail.com>
2022-05-04 10:56:23 +0200

    createdFunctionCountryButtons
    #61 finitio

--- createFunctionCountryButtons.js
+++ createFunctionCountryButtons.js
@@ -1,38 +1,59 @@
 "use strict";
 
-let countries= document.createElement("div");
-countries.innerText = "Argentina";
-document.body.appendChild(countries);
+const COUNTRYBUTTON = [
+    {
+        "id": 0,
+        "name": "Spain"
+    },
+    {
+        "id": 1,
+        "name": "France"
+    },
+    {
+        "id": 2,
+        "name": "Australia"
+    },
+    {
+        "id": 3,
+        "name": "United Kingdom"
+    },
+    {
+        "id": 4,
+        "name": "Sweden"
+    },
+    {
+        "id": 5,
+        "name": "Mexico"
+    },
+    {
+        "id": 6,
+        "name": "USA"
+    },
+    {
+        "id": 7,
+        "name": "Argentina"
+    },
+    {
+        "id": 8,
+        "name": "Chile"
+    },
     
-let countries1= document.createElement("div");
-countries1.innerText = "Australien";
-document.body.appendChild(countries1);
+];
 
-let countries2= document.createElement("div");
-countries2.innerText = "Chile";
-document.body.appendChild(countries2);
 
-let countries3= document.createElement("div");
-countries3.innerText = "Frankrike";
-document.body.appendChild(countries3);
+const countryButtons = document.getElementById("countryButtons");
 
-let countries4= document.createElement("div");
-countries4.innerText = "Mexico";
-document.body.appendChild(countries4);
+function createCountryButtons (COUNTRYBUTTON) {
+    for (let i = 0; i < COUNTRYBUTTON.length; i++) {
+        const button = document.createElement("button");
+        button.innerText = ` ${COUNTRYBUTTON[i].name} `;
+        button.addEventListener("click", function() {
+            console.log(i)
+        })
+        countryButtons.appendChild(button);
+        }
 
-let countries5= document.createElement("div");
-countries5.innerText = "Spanien";
-document.body.appendChild(countries5);
+}
 
-let countries6= document.createElement("div");
-countries6.innerText = "Storbritannien";
-document.body.appendChild(countries6);
-
-let countries7= document.createElement("div");
-countries7.innerText = "Sverige";
-document.body.appendChild(countries7);
-
-let countries8= document.createElement("div");
-countries8.innerText = "Usa";
-document.body.appendChild(countries8);
- 
+createCountryButtons (COUNTRYBUTTON);
+    

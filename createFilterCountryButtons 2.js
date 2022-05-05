a328f0db7d4b4c5066243383c831205fb0bd173e
Joakim Fröjvall Bejram <95027528+Jocke1999@users.noreply.github.com>
2022-05-05 15:04:19 +0200

    Create function filter country buttons (#94)
    * createFunctionFilter
    
    Filtrering klar, lägga in bilder.
    
    * createFunctionFilterCountry
    
    Fått in bilder på plats. Fortsätter med denna
    
    Co-authored-by: Sarah Flodin Jakobsson <95027610+SarahFlodin@users.noreply.github.com>

---
+++ createFilterCountryButtons 2.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function filterButtons (database)
+if (countryID == cities.countryID)
+return CITIES.name + CITIES.imageNormal;
+
+remove(template);
+
+element.remove();
--- createFilterCountryButtons.js
+++ createFilterCountryButtons.js
@@ -11,16 +11,17 @@ function createFilterElements (cities) {
     citiesDiv.innerHTML = "";
         for (let i = 0; i < cities.length; i++) {
             let div = document.createElement("div");
+            let image = document.createElement("img");
+            div.textContent = `${cities[i].name}`;
+            image.src = `./Images/${cities[i].imagesNormal[0]}`;
+            image.style.height = "5vw"
+            div.append (image)
+            citiesDiv.append(div);
+        }
+}
             div.textContent = `${cities[i].name}`;
             citiesDiv.append(div);
             
         }
 }
 
-// function filterButtons (database)
-// if (countryID == cities.countryID)
-// return CITIES.name + CITIES.imageNormal;
-
-// remove(template);
-
-// element.remove();

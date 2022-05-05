c8f43e9b9d7079f6e38b3d9c3e67686a6dd837a4
melissasjostedt <97900446+melissasjostedt@users.noreply.github.com>
2022-05-05 14:40:35 +0200

    65 build function filterbuttons (#93)
    * Filter subjects - not done
    
    * filter
    
    -
    
    Co-authored-by: Sarah Flodin Jakobsson <95027610+SarahFlodin@users.noreply.github.com>

---
+++ filterProgrammeButtons.js
@@ -0,0 +1,21 @@
+"use script"
+
+function filterProgrammeButtons(id) {
+   return SUBJECTS.filter(subject => subject.programmeID == id);
+
+}
+
+function createFilterProgrammeElements (subjects){
+   console.log (subjects);
+   let subjectsDiv = document.querySelector("#subjects")
+   subjectsDiv.innerHTML ="";
+    for (let i =0; i < subjects.length; i++) {
+     let div = document.createElement("div");
+     div.textContent = `${subjects[i].name}`;
+      subjectsDiv.append(div);
+   }
+}
+
+
+
+

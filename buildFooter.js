c9e26c6123827d19b671d83384f393d1ac11ccf2
JuliaErmansson <97975351+JuliaErmansson@users.noreply.github.com>
2022-05-05 13:50:20 +0200

    function buildFooter (#87)
    #58
    
    Co-authored-by: Sarah Flodin Jakobsson <95027610+SarahFlodin@users.noreply.github.com>

---
+++ buildFooter.js
@@ -0,0 +1,25 @@
+"use strict"
+/*function BuildFooter ()
+creatediv aboutUs
+	append textcontent “Om oss …”
+	array aboutUs()
+	
+return aboutUs*/
+
+function buildFooter(){
+    let aboutUs = document.getElementById("footer");
+    aboutUs.innerHTML = "<h3> Om oss </h3> Vi är fyra kreativa studenter som läser progrmmering på Malmö universitet. Vi ville skapa en app för andra studenter som är intresserade av att bli utbytesstudenter. En app som är lätt att navigera på och som ger mycket information om utbudet." 
+    for (let i = 0; i < ABOUTUS.length; i++){
+    let aboutUsImages = document.createElement("div");
+    let image = document.createElement("img");
+    image.src = `./Images/${ABOUTUS[i].image}`;
+    aboutUsImages.innerHTML =  `${ABOUTUS[i].name}`;
+    aboutUs.append(aboutUsImages);
+    aboutUsImages.append(image);
+    image.style.height = "5vw"
+    image.style.borderRadius = "50%"
+    }
+    return aboutUs;
+}
+
+buildFooter();

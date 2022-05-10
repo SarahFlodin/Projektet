7f4a182cdd0081f46aacf3c553038244c69e33e1
melissasjostedt <97900446+melissasjostedt@users.noreply.github.com>
2022-05-10 12:33:28 +0200

    Fix programmefilter (#103)
    * Filter subjects - not done
    
    * filter
    
    -
    
    * fixed programme filter, edited in createFieldButton
    
    #65
    
    Co-authored-by: Sarah Flodin Jakobsson <95027610+SarahFlodin@users.noreply.github.com>

--- createFilterCountryButtons.js
+++ createFilterCountryButtons.js
@@ -11,6 +11,20 @@ function createFilterElements (cities) {
     citiesDiv.innerHTML = "";
         for (let i = 0; i < cities.length; i++) {
             let div = document.createElement("div");
+
+            div.textContent = `${cities[i].name}`;
+            citiesDiv.append(div);
+            
+        }
+}
+
+// function filterButtons (database)
+// if (countryID == cities.countryID)
+// return CITIES.name + CITIES.imageNormal;
+
+// remove(template);
+
+// element.remove();
             let image = document.createElement("img");
             div.textContent = `${cities[i].name}`;
             image.src = `./Images/${cities[i].imagesNormal[0]}`;
@@ -19,3 +33,4 @@ function createFilterElements (cities) {
             citiesDiv.append(div);
         }
 }
+

eb384c7153606c68911a745e503b567d013b9fc1
Sarah Flodin Jakobsson <95027610+SarahFlodin@users.noreply.github.com>
2022-05-10 22:37:11 +0200

    fieldButtonDone (#109)

--- filterProgrammeButtons.js
+++ filterProgrammeButtons.js
@@ -1,23 +1,34 @@
 "use script"
 
 
-function filterProgrammeButtons(id) {
-  
-   return PROGRAMMES.filter(programme => programme.subjectID == id);
+function filterProgrammeButtons(buttonObject, FIELDSBUTTON) {
 
+      if (buttonObject.selected) {
+         buttonObject.selected = false;
+      } else {
+         buttonObject.selected = true;
+      }
+
+   console.log(FIELDSBUTTON);
+   
+   let fieldArray = FIELDSBUTTON.filter(fieldbutton => fieldbutton.selected);
+   
+   let idArray = fieldArray.map(element => element.id);
+
+   console.log(idArray);
+
+   return PROGRAMMES.filter(programme => idArray.includes(programme.subjectID));
+   
 }
 
 
 function createFilterProgrammeElements (subjects){
    console.log (subjects);
-   let subjectsDiv = document.querySelector("#subjects")
-   subjectsDiv.innerHTML ="";
-    for (let i =0; i < subjects.length; i++) {
+   let subjectsDiv = document.querySelector("#subjects");
+   subjectsDiv.innerHTML = "";
+    for (let i = 0; i < subjects.length; i++) {
      let div = document.createElement("div");
      div.innerHTML = `<h3>${subjects[i].name}</h3> <p> Nivå:${subjects[i].level}</p> <br><br> <p>Stad:${subjects[i].universityID}</p>`;
       subjectsDiv.append(div);
    }
 }
-
-
-

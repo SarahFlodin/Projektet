5861205fc919359584dae329cdd6e9f7e295f222
Sarah Flodin Jakobsson <sarahflodinjakobsson@gmail.com>
2022-05-10 14:06:33 +0200

    fixes

--- filterProgrammeButtons.js
+++ filterProgrammeButtons.js
@@ -1,4 +1,3 @@
-
 "use script"
 
 function filterProgrammeButtons(id) {
@@ -17,4 +16,15 @@ function createFilterProgramme (programmes) {
             div.innerHTML = `<h3>${programmes[i].name}</h3> <p>Nivå:${programmes[i].level}</p> <br><br> <p>Stad:${programmes[i].universityID}</p>`;
             programmesDiv.append(div);
         }
+}
+
+function createFilterProgrammeElements (subjects){
+   console.log (subjects);
+   let subjectsDiv = document.querySelector("#subjects")
+   subjectsDiv.innerHTML ="";
+    for (let i =0; i < subjects.length; i++) {
+     let div = document.createElement("div");
+     div.textContent = `${subjects[i].name}`;
+      subjectsDiv.append(div);
+   }
 }

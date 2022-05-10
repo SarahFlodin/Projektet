8f88fcb4de955eab65286cadde28dee1027006c1
Sarah Flodin Jakobsson <95027610+SarahFlodin@users.noreply.github.com>
2022-05-10 12:44:33 +0200

    fixFunctions (#104)

--- filterProgrammeButtons.js
+++ filterProgrammeButtons.js
@@ -6,23 +6,6 @@ function filterProgrammeButtons(id) {
 
 }
 
-
-
-function createFilterProgramme (programmes) {
-   
-    let programmesDiv = document.querySelector("#programme");
-    programmesDiv.innerHTML = "";
-        for (let i = 0; i < programmes.length; i++) {
-            let div = document.createElement("div");
-            div.innerHTML = `<h3>${programmes[i].name}</h3> <p>Nivå:${programmes[i].level}</p> <br><br> <p>Stad:${programmes[i].universityID}</p>`;
-            programmesDiv.append(div);
-        }
-}
-
-   return SUBJECTS.filter(subject => subject.programmeID == id);
-
-}
-
 function createFilterProgrammeElements (subjects){
    console.log (subjects);
    let subjectsDiv = document.querySelector("#subjects")

16f15528ec28b384281a8eb90a866688c86fd80e
Sarah Flodin Jakobsson <sarahflodinjakobsson@gmail.com>
2022-05-11 09:29:44 +0200

    Update filterProgrammeButtons.js

--- filterProgrammeButtons.js
+++ filterProgrammeButtons.js
@@ -12,20 +12,20 @@ function filterProgrammeButtons(buttonObject, FIELDSBUTTON) {
    console.log(FIELDSBUTTON);
    
    let fieldArray = FIELDSBUTTON.filter(fieldbutton => fieldbutton.selected);
-   
    let idArray = fieldArray.map(element => element.id);
-
-   console.log(idArray);
 
    return PROGRAMMES.filter(programme => idArray.includes(programme.subjectID));
    
 }
 
 
+
 function createFilterProgrammeElements (subjects){
    console.log (subjects);
+   
    let subjectsDiv = document.querySelector("#subjects");
    subjectsDiv.innerHTML = "";
+
     for (let i = 0; i < subjects.length; i++) {
      let div = document.createElement("div");
      div.innerHTML = `<h3>${subjects[i].name}</h3> <p> Nivå:${subjects[i].level}</p> <br><br> <p>Stad:${subjects[i].universityID}</p>`;

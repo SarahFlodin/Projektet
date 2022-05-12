3d526338b930ccf43da59c0cdd9bab36736ce98f
Sarah Flodin Jakobsson <95027610+SarahFlodin@users.noreply.github.com>
2022-05-12 14:58:52 +0200

    overlayStartHamburgerFixes (#124)

--- createFilterCountryButtons.js
+++ createFilterCountryButtons.js
@@ -7,15 +7,16 @@ function filterButtons (id) {
 
 function createFilterElements (cities) {
     console.log (cities);
-    let citiesDiv = document.querySelector("#cities")
+    let citiesDiv = document.querySelector("#cities");
     citiesDiv.innerHTML = "";
         for (let i = 0; i < cities.length; i++) {
             let div = document.createElement("div");
+            div.classList.add("cityBox");
             let image = document.createElement("img");
             div.textContent = `${cities[i].name}`;
             image.src = `./Images/${cities[i].imagesNormal[0]}`;
         image.style.height = "5vw"
-            div.append (image)
+            div.append (image);
             citiesDiv.append(div);
         }
 }

afae813ade079d48c9aa87754a7497a2423538fa
Sarah Flodin Jakobsson <sarahflodinjakobsson@gmail.com>
2022-05-17 14:40:30 +0200

    cssFixes

--- createCitiesFilterElements.js
+++ createCitiesFilterElements.js
@@ -17,7 +17,7 @@ function createCitiesFilterElements (cities) {
             let image = document.createElement("img");
             div.textContent = `${cities[i].name}`;
             image.src = `./Images/${cities[i].imagesNormal[0]}`;
-        image.style.height = "5vw"
+
             div.append (image);
             citiesDiv.append(div);
         }

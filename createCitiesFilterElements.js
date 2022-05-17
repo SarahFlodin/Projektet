a5514ebd9373bb1d05857944b48cf18c71bc4594
Joakim Fröjvall Bejram <joakimfrojram@gmail.com>
2022-05-17 17:18:39 +0200

    clickOnMultipleButtonsOnCountries
    Man kan nu klicka på de olika länderna

--- createCitiesFilterElements.js
+++ createCitiesFilterElements.js
@@ -8,7 +8,6 @@ function filterButtons (id) {
 
 // Det är denna som skapar alla boxarna till städerna 
 function createCitiesFilterElements (cities) {
-    console.log (cities);
     let citiesDiv = document.querySelector("#cities");
     citiesDiv.innerHTML = "";
         for (let i = 0; i < cities.length; i++) {

eec4714ea14d66f97c84b780a7ea12254c9c6b0e
Joakim Fröjvall Bejram <95027528+Jocke1999@users.noreply.github.com>
2022-05-17 17:29:34 +0200

    clickOnMultipleButtonsOnCountries (#140)
    Man kan nu klicka på de olika länderna
    
    Co-authored-by: Sarah Flodin Jakobsson <95027610+SarahFlodin@users.noreply.github.com>

--- createFunctionCountryButtons.js
+++ createFunctionCountryButtons.js
@@ -5,39 +5,48 @@
 const COUNTRYBUTTON = [
     {
         "id": 0,
-        "name": "Spain"
+        "name": "Spain",
+        "selected": false
     },
     {
         "id": 1,
-        "name": "France"
+        "name": "France",
+        "selected": false
     },
     {
         "id": 2,
-        "name": "Australia"
+        "name": "Australia",
+        "selected": false
     },
     {
         "id": 3,
-        "name": "United Kingdom"
+        "name": "United Kingdom",
+        "selected": false
     },
     {
         "id": 4,
-        "name": "Sweden"
+        "name": "Sweden",
+        "selected": false
     },
     {
         "id": 5,
-        "name": "Mexico"
+        "name": "Mexico",
+        "selected": false
     },
     {
         "id": 6,
-        "name": "USA"
+        "name": "USA",
+        "selected": false
     },
     {
         "id": 7,
-        "name": "Argentina"
+        "name": "Argentina",
+        "selected": false
     },
     {
         "id": 8,
-        "name": "Chile"
+        "name": "Chile",
+        "selected": false
     },
     
 ];
@@ -53,7 +62,7 @@ function createCountryButtons (COUNTRYBUTTON) {
         button.innerText = ` ${COUNTRYBUTTON[i].name} `;
 
         button.addEventListener("click", function() {
-            let foundCities = filterButtons (i);
+            let foundCities = filterCountryButtons (COUNTRYBUTTON[i],COUNTRYBUTTON);
             createCitiesFilterElements (foundCities);
             button.classList.toggle("button-active");
         })
@@ -62,5 +71,40 @@ function createCountryButtons (COUNTRYBUTTON) {
 
 }
 
+
 createCountryButtons (COUNTRYBUTTON);
 
+function filterCountryButtons(buttonObject, COUNTRYBUTTON) {
+
+    if (buttonObject.selected) {
+       buttonObject.selected = false;
+    } else {
+       buttonObject.selected = true;
+    }
+
+ console.log(COUNTRYBUTTON);
+ 
+ let countryArray = COUNTRYBUTTON.filter(countrybutton => countrybutton.selected);
+ let idArray = countryArray.map(element => element.id);
+ return CITIES.filter (city => idArray.includes(city.countryID));
+
+
+
+}
+
+// Det är denna som skapar alla boxarna till städerna 
+function createCitiesFilterElements (cities) {
+    let citiesDiv = document.querySelector("#cities");
+    citiesDiv.innerHTML = "";
+        for (let i = 0; i < cities.length; i++) {
+            let div = document.createElement("div");
+            div.classList.add("cityBox");
+            let image = document.createElement("img");
+            div.textContent = `${cities[i].name}`;
+            image.src = `./Images/${cities[i].imagesNormal[0]}`;
+
+            div.append (image);
+            citiesDiv.append(div);
+        }
+        console.log(cities)
+}

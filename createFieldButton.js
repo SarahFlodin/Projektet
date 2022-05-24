1f4b0171bb73d729380992e895e91d08321770e9
Joakim Fröjvall Bejram <95027528+Jocke1999@users.noreply.github.com>
2022-05-24 11:24:11 +0200

    createFieldButtonComments (#200)

--- createFieldButton.js
+++ createFieldButton.js
@@ -1,6 +1,11 @@
 'use script'
 
 // Knapparna till ämnen på ProgrammePage
+//Skapar en funktion vid namn createFieldButtons med parameterns (FIELDSBUTTON)
+//deklarerar createFieldButtons och hämtar Idet "fieldButtons"
+//skapar en loop där vi inkluderar en deklarerad "button" vars skapar en klickbar knapp
+//knappen innehåller text ifrån FIELDSBUTTON där vi inuti loopen kallar på "korrekt" ämne. 
+//sedan skapar vi en eventListener så att man kan klicka på "button"
 function createFieldButtons (FIELDSBUTTON) {
   let fieldButtons = document.getElementById('fieldButtons')
 
@@ -14,7 +19,7 @@ function createFieldButtons (FIELDSBUTTON) {
         FIELDSBUTTON
       )
       createFilterProgrammeElements(foundProgrammes)
-      button.classList.toggle('button-active')
+      button.classList.toggle('button-active') //Skapar en "toggle" för när knappen är nedryckt
     })
     fieldButtons.appendChild(button)
   }
